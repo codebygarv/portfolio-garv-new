8ae2e58f77362bf4271c550dbefe4260d69863de
Garv Thakral <garvthakral90@gmail.com>
2025-03-09 19:25:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const connectDB = require('./db/db.js');
 
 connectDB();
 
+const path = require('path');
+const connectDB = require(path.join(__dirname, 'db', 'db.js'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
