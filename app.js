930a362e83e3ee1da819538370be414ce79a747c
Garv Thakral <garvthakral90@gmail.com>
2025-03-09 18:21:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/user.routes');
-const connectDB = require('./db/db');
+// const connectDB = require('./db/db');
 
-connectDB();
+// connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
